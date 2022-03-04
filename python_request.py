530aaef4ecd16d6aa440d640fda1121db28bfc9e
Arno Hartmann <arno.philipp.hartmann@gmail.com>
2022-03-04 14:23:08 +0100

    Changed country to Barbados

--- python_request.py
+++ python_request.py
@@ -4,7 +4,7 @@ from writeJSON import writeJSONFile
 #Aufgabe: 
 
 
-url = requests.get("https://api.covid19api.com/live/country/switzerland/status/confirmed")
+url = requests.get("https://api.covid19api.com/live/country/barbados/status/confirmed")
 
 #response is an array of dicts
 response = url.json()
@@ -12,7 +12,7 @@ response = url.json()
 # x = (len(response))-1
 # y = response[-7]
 
-cases = response[-1]['Confirmed'] - response[-8]['Confirmed']
+cases = response[-1]['Confirmed']
 active = response[-1]['Active'] - response[-8]['Active']
 
 print(cases, active)
